11e8b6d420c165372c18c6784cc0dc168c6b332b
moguimon <74788890+moguimon@users.noreply.github.com>
2021-02-11 10:26:46 +0100

    Add files via upload

--- codigo_ajax.js
+++ codigo_ajax.js
@@ -7,10 +7,9 @@ var estados=['No inicializado', 'Cargando', 'Cargado', 'Interactivo', 'Completad
 var tiempo_inic = 0;
 
 // Borra lo que hay en los contenedores
-var mirecurso = document.getElementById("recurso");
-mirecurso.value=location.href;
-document.getElementById('contenidos').innerHTML = "";
-document.getElementById('estados').innerHTML = "";
+
+//document.getElementById('contenidos').innerHTML = "";
+//document.getElementById('estados').innerHTML = "";
 // Creo el XHR y realizo la solicitud al servidor
 if(window.XMLHttpRequest) {
   solicitud = new XMLHttpRequest();
